426f2a187d55bd1b95646d2b7ec8cb0f0947673d
PrintJenius <uopl178@naver.com>
2025-08-28 18:09:54 +0900

    axios 기본 URL 설정: 백엔드 서버 주소 지정 - main.jsx에 axios import 추가 - axios.defaults.baseURL = 'http://tftshare.com:8080' 설정 - 모든 API 호출이 백엔드 서버로 전송되도록 수정

--- tft-share-frontend/src/main.jsx
+++ tft-share-frontend/src/main.jsx
@@ -2,8 +2,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import axios from 'axios';
 
 const clientId = '221594470812-2msddcnuckjr7iklc7j749epek0pbbjr.apps.googleusercontent.com';
+
+// axios 기본 URL 설정 (백엔드 서버)
+axios.defaults.baseURL = 'http://tftshare.com:8080';
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientId}>
