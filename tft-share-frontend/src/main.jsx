3aa290243482634dc90a580439ab7582c6a7b37b
PrintJenius <uopl178@naver.com>
2025-08-28 18:17:35 +0900

    axios 기본 URL을 HTTPS로 변경: Mixed Content 에러 해결

--- tft-share-frontend/src/main.jsx
+++ tft-share-frontend/src/main.jsx
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 const clientId = '221594470812-2msddcnuckjr7iklc7j749epek0pbbjr.apps.googleusercontent.com';
 
-// axios 기본 URL 설정 (백엔드 서버)
-axios.defaults.baseURL = 'http://tftshare.com:8080';
+// axios 기본 URL 설정 (백엔드 서버) - HTTPS 사용
+axios.defaults.baseURL = 'https://tftshare.com:8080';
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientId}>
