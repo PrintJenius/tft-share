c2ead80ec7dc045d66f0dc28d629050b662f098f
PrintJenius <uopl178@naver.com>
2025-09-01 12:28:30 +0900

    fix: 메인 메서드 수정 - env파일에서 가져오는것이 아닌 시스템 환경변수에서 가져오도록 하기 위함

--- tft-share-backend/tft-share/src/main/java/com/neojen/tft_share/TftShareApplication.java
+++ tft-share-backend/tft-share/src/main/java/com/neojen/tft_share/TftShareApplication.java
@@ -9,43 +9,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TftShareApplication {
 
     public static void main(String[] args) {
-
-        // env 파일 선택: ENVIRONMENT 환경변수에 따라 dev/prod 결정
-        String envType = System.getenv("ENVIRONMENT"); // 운영체제 ENVIRONMENT 우선
-        if (envType == null || envType.isEmpty()) {
-            envType = "dev"; // 기본값 dev
-        }
-
-        String dotenvFile = ".env." + envType;
-
-        // 2Dotenv 로드
-        Dotenv dotenv = Dotenv.configure()
-                               .filename(dotenvFile)
-                               .ignoreIfMissing()
-                               .load();
-
-        // 3환경 변수 읽기
-        String dbUsername = dotenv.get("DB_USERNAME");
-        String dbPassword = dotenv.get("DB_PASSWORD");
-        String googleClientId = dotenv.get("GOOGLE_CLIENT_ID");
-        String googleClientSecret = dotenv.get("GOOGLE_CLIENT_SECRET");
-        String jwtSecret = dotenv.get("JWT_SECRET");
-        String riotApiKey = dotenv.get("RIOT_API_KEY");
-        String awsAccessKey = dotenv.get("AWS_ACCESS_KEY");
-        String awsSecretKey = dotenv.get("AWS_SECRET_KEY");
-        
-        System.setProperty("DB_USERNAME", dbUsername);
-        System.setProperty("DB_PASSWORD", dbPassword);
-        System.setProperty("GOOGLE_CLIENT_ID", googleClientId);
-        System.setProperty("GOOGLE_CLIENT_SECRET", googleClientSecret);
-        System.setProperty("JWT_SECRET", jwtSecret);
-        System.setProperty("RIOT_API_KEY", riotApiKey);
-        System.setProperty("AWS_ACCESS_KEY", awsAccessKey);
-        System.setProperty("AWS_SECRET_KEY", awsSecretKey);
-
-        // 5️⃣ SpringApplication 실행 + profile 적용
-        SpringApplication app = new SpringApplication(TftShareApplication.class);
-        app.setAdditionalProfiles(envType); // dev / prod profile 적용
-        app.run(args);
+        SpringApplication.run(TftShareApplication.class, args);
     }
 }
