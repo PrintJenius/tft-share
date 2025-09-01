27839235544f2fb841556730d0647e51d54b3849
PrintJenius <uopl178@naver.com>
2025-09-01 14:57:00 +0900

    remove: 게시판 관련 엔티티 및 테이블 제거

--- tft-share-backend/tft-share/src/main/java/com/neojen/tft_share/repository/BoardCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neojen.tft_share.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.neojen.tft_share.entity.BoardCategory;
-
-public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long>{
-
-}
